4d3954fdd4ce249565298e63dfe48276b6f30cd1
Samuel Li <sam@cisl-m121a>
2024-10-05 16:49:16 -0700

    add functions to check if an input array really has the specified type of missing values

--- include/h5zsperr_helper.h
+++ include/h5zsperr_helper.h
@@ -5,13 +5,25 @@
 #ifndef H5ZSPERR_HELPER_H
 #define H5ZSPERR_HELPER_H
 
+#include <stdlib.h>
+
+#define LARGE_MAGNITUDE 1e35
+
 /*
- * Pack additional information about the input data into an `unsigned int`.
+ * Pack and unpack additional information about the input data into an integer.
  * It returns the encoded unsigned int, which shouldn't be zero.
  * The packing function is called by `set_local()` to prepare information
  * for `H5Z_filter_sperr()`, which calls the unpack function.
  */
-unsigned int h5zsperr_pack_data_type(int rank, int dtype);
-void h5zsperr_unpack_data_type(unsigned int meta, int* rank, int* dtype);
+unsigned int h5zsperr_pack_data_type(int rank, int is_float);
+void h5zsperr_unpack_data_type(unsigned int meta, int* rank, int* is_float);
+
+/*
+ * Check if an input array really has missing values.
+ */
+int h5zsperr_has_nan(const void* buf, size_t nelem, int is_float);
+int h5zsperr_has_large_mag(const void* buf, size_t nelem, int is_float);
+int h5zsperr_has_specific_f32(const void* buf, size_t nelem, float val);
+int h5zsperr_has_specific_f64(const void* buf, size_t nelem, double val);
 
 #endif
--- src/h5zsperr_helper.c
+++ src/h5zsperr_helper.c
@@ -1,9 +1,10 @@
 #include "h5zsperr_helper.h"
 #include <assert.h>
+#include <math.h> /* isnan() */
 
 #include <H5PLextern.h>
 
-unsigned int h5zsperr_pack_data_type(int rank, int dtype)
+unsigned int h5zsperr_pack_data_type(int rank, int is_float)
 {
   unsigned int ret = 0;
 
@@ -24,15 +25,15 @@ unsigned int h5zsperr_pack_data_type(int rank, int dtype)
    * Bit position 4-7 encode data type.
    * Only float (1) and double (0) are supported right now.
    */
-  if (dtype == 1)   /* is_float   */
+  if (is_float == 1)
     ret |= 1u << 4; /* Position 4 */
   else
-    assert(dtype == 0);
+    assert(is_float == 0);
 
   return ret;
 }
 
-void h5zsperr_unpack_data_type(unsigned int meta, int* rank, int* dtype)
+void h5zsperr_unpack_data_type(unsigned int meta, int* rank, int* is_float)
 {
   /*
    * Extract rank from bit positions 0-3.
@@ -54,7 +55,65 @@ void h5zsperr_unpack_data_type(unsigned int meta, int* rank, int* dtype)
    */
   unsigned pos4 = meta & (1u << 4);
   if (pos4)
-    *dtype = 1; /* is_float  */
+    *is_float = 1;
   else
-    *dtype = 0; /* is_double */
+    *is_float = 0; /* is_double */
+}
+
+int h5zsperr_has_nan(const void* buf, size_t nelem, int is_float)
+{
+  assert(is_float == 1 || is_float == 0);
+  if (is_float) {
+    const float* p = (const float*)buf;
+    for (size_t i = 0; i < nelem; i++)
+      if (isnan(p[i]))
+        return 1;
+  }
+  else {
+    const double* p = (const double*)buf;
+    for (size_t i = 0; i < nelem; i++)
+      if (isnan(p[i]))
+        return 1;
+  }
+
+  return 0;
+}
+
+int h5zsperr_has_large_mag(const void* buf, size_t nelem, int is_float)
+{
+  assert(is_float == 1 || is_float == 0);
+  if (is_float) {
+    const float* p = (const float*)buf;
+    for (size_t i = 0; i < nelem; i++)
+      if (fabsf(p[i]) >= LARGE_MAGNITUDE)
+        return 1;
+  }
+  else {
+    const double* p = (const double*)buf;
+    for (size_t i = 0; i < nelem; i++)
+      if (fabs(p[i]) >= LARGE_MAGNITUDE)
+        return 1;
+  }
+
+  return 0;
+}
+
+int h5zsperr_has_specific_f32(const void* buf, size_t nelem, float val)
+{
+  const float* p = (const float*)buf;
+  for (size_t i = 0; i < nelem; i++)
+    if (p[i] == val)
+      return 1;
+
+  return 0;
+}
+
+int h5zsperr_has_specific_f64(const void* buf, size_t nelem, double val)
+{
+  const double* p = (const double*)buf;
+  for (size_t i = 0; i < nelem; i++)
+    if (p[i] == val)
+      return 1;
+
+  return 0;
 }
